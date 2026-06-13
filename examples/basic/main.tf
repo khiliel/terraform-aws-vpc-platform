98c47d45ed5b643230fdc87167b0bef1581c4fc6
khiliel <khilielbullock@gmail.com>
2026-06-12 20:45:23 -0400

    refactor: added terraform block to examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0, < 7.0"
+    }
+  }
 }
+
 
 module "vpc" {
   source = "../.."
